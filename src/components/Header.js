3f0e6cd2f2fd8df94d1706b2f9744aac1d472399
Qiydaarwilliams23@gmail.com <Qiydaarwilliams23@gmail.com>
2024-09-08 19:35:33 -0400

    frontend put connected

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <h1>Our Header</h1>
+    <div className='app-header'>
+      <h1>Note List</h1>
+    </div>
+
   )
 }
 
--- src/components/pages/Notepage.js
+++ src/components/pages/Notepage.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const NotePage = () => {
     const { id: noteId } = useParams();
     const [note, setNote] = useState(null);
+
+    const Navigate = useNavigate()
 
     useEffect(() => {
         const getNote = async () => {
@@ -19,8 +22,40 @@ const NotePage = () => {
         getNote();
     }, [noteId]);
 
+
+        const updateNote = async () => {
+            try {
+                const response = await fetch(`/api/notes/${noteId}/update/`,{
+                    method: "PUT",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body:JSON.stringify(note)
+                });
+                const data = await response.json();
+                setNote(data);
+            } catch (error) {
+                console.error('Failed to fetch the note:', error);
+            }
+        };
+
+        const handleSubmit = (e) => {
+            e.preventDefault()
+            updateNote()
+            Navigate("/")
+        }
+
     return (
-        <p>{note?.body}</p>
+        <div className='note'>
+            <div className='note-header'>
+               <Link to="/">
+               <h1 onClick={handleSubmit}>(Back)</h1>
+               </Link>
+
+            </div>
+             <textarea onChange={(e) =>{setNote({...note, 'body':e.target.value })}}  defaultValue={note?.body}></textarea>
+        </div>
+
     );
 };
 
